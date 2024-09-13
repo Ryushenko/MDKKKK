39ff7c265a02134d51c0ba262f2383e2383789e3
Kirill <153394435+Ryushenko@users.noreply.github.com>
2024-09-13 14:02:18 +0400

    Add files via upload

---
+++ TeamGitapp5/TeamGitapp5/Program.cs
@@ -0,0 +1,26 @@
+﻿while (true)
+{
+    Console.WriteLine("Введи число!");
+    double a = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введи число!");
+    double b = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите арифметический знак");
+    var c = Console.ReadLine();
+
+    if (c == "/")
+    {
+        Console.WriteLine(a / b);
+    }
+    else if (c == "+")
+    {
+        Console.WriteLine(a + b);
+    }
+    else if (c == "-")
+    {
+        Console.WriteLine(a - b);
+    }
+    else if (c == "*")
+    {
+        Console.WriteLine(a * b);
+    }
+}
