e1f46122f2b90c55da61bc4a1c7138e7d1bbf4a6
Kirill <153394435+Ryushenko@users.noreply.github.com>
2024-09-13 14:01:39 +0400

    Add files via upload

---
+++ TeamGitapp4/TeamGitapp4/Program.cs
@@ -0,0 +1,8 @@
+﻿while (true)
+{
+    Console.WriteLine("Введи число!");
+    double a = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введи число!");
+    double b = double.Parse(Console.ReadLine());
+    Console.WriteLine(a / b);
+}
